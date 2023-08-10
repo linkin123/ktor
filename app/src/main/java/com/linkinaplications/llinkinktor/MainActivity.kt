9854ddc46b0f65e8e3b59fed59752994601a3ed3
luis <linkinluisave@gmail.com>
2023-08-10 00:24:25 -0600

    added changes in view: image style crop and size of texts.

--- app/src/main/java/com/linkinaplications/llinkinktor/MainActivity.kt
+++ app/src/main/java/com/linkinaplications/llinkinktor/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -23,17 +18,14 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
 import com.linkinaplications.llinkinktor.data.remote.HttpRoutes.API_KEY
 import com.linkinaplications.llinkinktor.data.remote.PostsService
-import com.linkinaplications.llinkinktor.data.remote.dto.Movie
 import com.linkinaplications.llinkinktor.data.remote.dto.MovieList
-import com.linkinaplications.llinkinktor.data.remote.dto.PostResponse
 import com.linkinaplications.llinkinktor.ui.theme.LlinkinKtorTheme
 
 class MainActivity : ComponentActivity() {
@@ -65,14 +57,17 @@ class MainActivity : ComponentActivity() {
                                         )
                                 ) {
                                     AsyncImage(
-                                        model = "https://image.tmdb.org/t/p/w500/${it.poster_path}",
-                                        contentDescription = "poster"
+                                        model = "https://image.tmdb.org/t/p/w500/${it.backdrop_path}",
+                                        contentDescription = "poster",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        contentScale = ContentScale.Crop
                                     )
                                     Text(
                                         text = it.original_title,
-                                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                                        style = MaterialTheme.typography.titleMedium,
+                                        fontSize = 25.sp
                                     )
-
                                 }
                             }
                         }
